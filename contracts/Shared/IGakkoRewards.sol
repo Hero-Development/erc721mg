77b766570e939f64d00552330640711edd485254
Chris Esquibel <c.esquibel@herodev.com>
2022-11-15 14:45:12 -0700

    feat: add staking events and context info

--- contracts/Shared/IGakkoRewards.sol
+++ contracts/Shared/IGakkoRewards.sol
@@ -3,12 +3,12 @@
 pragma solidity ^0.8.4;
 
 struct StakeSummary{
-  uint32 started; //32
+  uint32 initial; //32
   uint32 total;   //64
   uint16 tokenId; //80
 }
 
 interface IGakkoRewards{
-  function handleRewards( address to, StakeSummary[] calldata claims ) external;
-  function handleStakes( uint16[] calldata tokenIds ) external;
+  function handleRewards(StakeSummary[] calldata claims) external;
+  function handleStakes(uint16[] calldata tokenIds) external;
 }
--- contracts/StakedBucketAuction.sol
+++ contracts/StakedBucketAuction.sol
@@ -1,13 +1,18 @@
 
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
 import "./BucketAuction.sol";
-import "./Shared/IGakkoLoot.sol";
 import "./Shared/IGakkoRewards.sol";
+import "./Shared/IStakingProvider.sol";
 
-contract StakedBucketAuction is BucketAuction{
+abstract contract StakedBucketAuction is IStakingProvider, BucketAuction{
+  event StakeClaimed( uint16 indexed tokenId, uint32 timestamp, uint32 collected );
+  event TokenStaked( uint16 indexed tokenId, uint32 timestamp );
+  event TokenUnstaked( uint16 indexed tokenId, uint32 timestamp );
+
   error IncorrectOwner();
+  error InvalidConfig();
   error InvalidStake();
   error NoTokensSpecified();
   error StakingInactive();
@@ -18,18 +23,10 @@ contract StakedBucketAuction is BucketAuction{
     uint32 total;   // 64
   }
 
-  struct StakeInfo{
-    uint16 tokenId;
-    uint32 accrued;
-    uint32 pending;
-    bool isStaked;
-  }
-
   uint32 public baseAward;
   bool public isStakeable;
-  IGakkoLoot public lootHandler;
   IGakkoRewards public rewardHandler;
-  mapping(uint256 => StakeData) public stakes;
+  mapping(uint16 => StakeData) public stakes;
 
   constructor(
     string memory collectionName,
@@ -59,74 +56,77 @@ contract StakedBucketAuction is BucketAuction{
 
 
   //nonpayable - public
-  function claimTokens( uint16[] calldata tokenIds, bool restake ) external {
-    if( tokenIds.length == 0 ) revert NoTokensSpecified();
+  function claimTokens(uint16[] calldata tokenIds, bool restake) external {
+    if (tokenIds.length == 0) revert NoTokensSpecified();
 
     uint256 length = tokenIds.length;
     uint32 time = uint32(block.timestamp);
     StakeSummary[] memory claims = new StakeSummary[](tokenIds.length);
-    for(uint256 i = 0; i < length; ++i ){
+    for (uint256 i = 0; i < length; ++i ) {
       //checks
       uint16 tokenId = tokenIds[i];
-      if( ERC721A.ownerOf( tokenId ) != msg.sender ) revert IncorrectOwner();
+      if (ERC721A.ownerOf( tokenId ) != msg.sender) revert IncorrectOwner();
 
       StakeData memory stake = stakes[ tokenId ];
-      if( stake.started < 2 ){
-        claims[ i ] = StakeSummary(
+      if (stake.started < 2) {
+        claims[i] = StakeSummary(
           stake.total,
           stake.total,
           tokenId
         );
       }
       else{
-        
         uint32 accrued = ( time - stake.started );
-        if( stake.total == 0 )
+        if (stake.total == 0)
           accrued += baseAward;
 
-
-        claims[ i ] = StakeSummary(
+        claims[i] = StakeSummary(
           stake.total,
           stake.total + accrued,
           tokenId
         );
 
-
         //effects
-        stakes[ tokenId ] = StakeData(
+        stakes[tokenId] = StakeData(
           restake ? time : 1,
           stake.total + accrued
         );
+
+        emit StakeClaimed( tokenId, time, accrued );
+
+        if(!restake)
+          emit TokenUnstaked(tokenId, time);
       }
     }
 
     //interactions
-    if( address(rewardHandler) != address(0) ){
-      rewardHandler.handleRewards( msg.sender, claims );
+    if(address(rewardHandler) != address(0)){
+      rewardHandler.handleRewards(claims);
     }
   }
 
   function stakeTokens( uint16[] calldata tokenIds ) external {
-    if( tokenIds.length == 0 ) revert NoTokensSpecified();
-    if( !isStakeable ) revert StakingInactive();
+    if (tokenIds.length == 0) revert NoTokensSpecified();
+    if (!isStakeable) revert StakingInactive();
 
+    uint256 length = tokenIds.length;
     uint32 time = uint32(block.timestamp);
-    for(uint256 i; i < tokenIds.length; ++i ){
+    for (uint256 i = 0; i < length; ++i) {
       //checks
       uint16 tokenId = tokenIds[i];
-      if( ERC721A.ownerOf(tokenId) != msg.sender ) revert IncorrectOwner();
+      if (ERC721A.ownerOf(tokenId) != msg.sender) revert IncorrectOwner();
 
       StakeData storage stake = stakes[ tokenId ];
-      if( stake.started > 1 ) continue;
-
-      //effects
-      stake.started = time;
+      if (stake.started < 2) {
+        //effects
+        stake.started = time;
+        emit TokenStaked( tokenId, time );
+      }
     }
 
     //interactions
-    if( address(rewardHandler) != address(0) ){
-      rewardHandler.handleStakes( tokenIds );
-    }
+    if(address(rewardHandler) != address(0))
+      rewardHandler.handleStakes(tokenIds);
   }
 
 
@@ -134,15 +134,10 @@ contract StakedBucketAuction is BucketAuction{
   function transferFrom(address from, address to, uint256 tokenId)
     public
     payable
-    override( ERC721A, IERC721A ) {
-    if( _isStaked(tokenId) ) revert TransferWhileStaked();
+    override(ERC721A, IERC721A) {
+    if (_isStaked(tokenId)) revert TransferWhileStaked();
 
-    super.transferFrom( from, to, tokenId );
-
-    //if transfer succeeded, pull is valid
-    if( address(lootHandler) != address(0) ){
-      lootHandler.handlePull( from, to, uint16(tokenId) );
-    }
+    ERC721A.transferFrom( from, to, tokenId );
   }
 
 
@@ -151,9 +146,8 @@ contract StakedBucketAuction is BucketAuction{
     baseAward = award;
   }
 
-  function setHandlers( IGakkoRewards reward, IGakkoLoot loot ) external onlyOwner{
-    rewardHandler = reward;
-    lootHandler = loot;
+  function setHandler( IGakkoRewards handler ) external onlyOwner{
+    rewardHandler = handler;
   }
 
   function setStakeable( bool stakeable ) external onlyOwner{
@@ -162,6 +156,10 @@ contract StakedBucketAuction is BucketAuction{
 
 
   //view - public
+  function getRewardHandler() external view returns(address){
+    return address(rewardHandler);
+  }
+
   function getStakeInfo( uint16[] calldata tokenIds ) external view returns (StakeInfo[] memory infos) {
     uint32 time = uint32(block.timestamp);
 
@@ -193,23 +191,30 @@ contract StakedBucketAuction is BucketAuction{
 
 
   //view - override
-  function ownerOf( uint256 tokenId )
+  function ownerOf(uint256 tokenId)
     public
     view
-    override( ERC721A, IERC721A )
-    returns( address currentOwner ){
+    override(ERC721A, IERC721A)
+    returns( address currentOwner ) {
     if (tokenId > type(uint16).max || !_exists(tokenId))
       revert URIQueryForNonexistentToken();
 
-    if( stakes[ uint16(tokenId) ].started > 1 )
+    if(_isStaked(tokenId))
       currentOwner = address(this);
     else
-      currentOwner = super.ownerOf( tokenId );
+      currentOwner = ERC721A.ownerOf(tokenId);
   }
 
+  function ownerOfAll(uint16[] calldata tokenIds) external view returns(address[] memory owners){
+    uint256 length = tokenIds.length;
+    owners = new address[]( length );
+    for(uint256 i = 0; i < length; ++i){
+      owners[i] = ERC721A.ownerOf(tokenIds[i]);
+    }
+  }
 
   //view - internal
   function _isStaked( uint256 tokenId ) internal view returns( bool ){
-    return stakes[ tokenId ].started > 1;
+    return stakes[uint16(tokenId)].started > 1;
   }
 }
